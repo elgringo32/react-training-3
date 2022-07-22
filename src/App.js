5c9131c2324595179efc7e6f30506bc1da2c97d2
Brian Knopke <BrianK@EDIT-2.local>
2022-07-22 10:50:55 -0700

    replaced state with hooks state

--- src/App.js
+++ src/App.js
@@ -2,50 +2,43 @@ import './App.css';
 import Header from './components/Header'
 import CardList from './components/CardList'
 import Scroll from './components/Scroll'
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import SearchBox from './components/SearchBox';
+import ErrorBoundry from './components/ErrorBoundry';
 
 
-class App extends Component  {
-  constructor() {
-    super()
-    this.state = {
-      robots: [],
-      searchfield: ''
-    }
-    console.log('constructor')
-  }
-
-  componentDidMount() {
+function App()  {
+  const [ robots, setRobots] = useState([])
+  const [ searchfield, setSearchfield] = useState('')
+  
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then((res) => res.json())
-    .then( users => this.setState({ robots: users}));
+      .then(res => res.json())
+      .then(users => setRobots(users));
+  }, [])
+
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value)    
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value })
-   
-    
-  }
-
-  render() {
-    const filteredRobots = this.state.robots.filter((robots) => {
-      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+  const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase())
     })
-    console.log('render')
 
-    return (
+  return !robots.length ?
+    <h1>Loading</h1> :
+      (
       <div className='tc'>
         <Header />
-        <SearchBox searchChange={this.onSearchChange}/>
+        <SearchBox searchChange={onSearchChange}/>
         <Scroll>
-        <CardList robots={filteredRobots}/>
+          <ErrorBoundry>
+            <CardList robots={filteredRobots}/>
+          </ErrorBoundry>
         </Scroll>
-        
       </div>
     )
-  }
-   
+
 }
 
 export default App;
